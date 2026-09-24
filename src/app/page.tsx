5060228107ae87efad7c7c00a2090a9a6cfacffa
Anish <anish67657@gmail.com>
2026-09-24 20:25:47 +0530

    Fetch tickets at request time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Charts from "./components/charts"
 import TicketTable from "./components/tickettable"
 import { Ticket } from "./type/types"
-
+export const dynamic = "force-dynamic"
 export default async function Home(){
   const res = await fetch(`${process.env.API_URL}/api/ticket`)
   const result: Ticket[] = await res.json()
